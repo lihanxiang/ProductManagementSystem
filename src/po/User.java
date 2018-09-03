10e25eb32410c95306f245e209079189b02eda5d
Hank <l945457758@gmail.com>
2018-09-03 10:27:53 +0800

    实体类

---
+++ src/po/User.java
@@ -0,0 +1,67 @@
+package po;
+
+public class User {
+    private String username;
+    private String password;
+    private String phone;
+    private String email;
+    private String gender;
+    private String description;
+    private String verifyCode;
+
+    /*public User(String username, String password, String phone, String email){
+        this.username = username;
+        this.password = password;
+        this.phone = phone;
+        this.email = email;
+    }*/
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getGender() {
+        return gender;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setVerifyCode(String verifyCode) {
+        this.verifyCode = verifyCode;
+    }
+    public String getVerifyCode() {
+        return verifyCode;
+    }
+}
