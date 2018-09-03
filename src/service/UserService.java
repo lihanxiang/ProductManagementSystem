2d97070cd4435d46961b08dea2120093289fe86e
Hank <l945457758@gmail.com>
2018-09-03 22:46:02 +0800

    Service 层

---
+++ src/service/UserService.java
@@ -0,0 +1,13 @@
+package service;
+
+import exception.UserException;
+import po.User;
+
+public interface UserService {
+    void addUser(User user) throws UserException;
+    void login(User user) throws UserException;
+    User showInfo(String username);
+    String getStatus(String username);
+    void setUserInfo(User user) throws UserException;
+    void verifyCode(String userCode, String verifyCode) throws UserException;
+}
